dd84fcac2759a592da757bc60492600f1d4f0ec6
zack <zedoov@hotmail.com>
2019-03-01 16:33:10 +1100

    fly uses rigidbody for movement

--- PhysicsAssignment/Assets/scripts/ball.cs
+++ PhysicsAssignment/Assets/scripts/ball.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ball : MonoBehaviour {
 
-    public float thrust;
+    private float thrust = 2;
 
     public Rigidbody m_rigidBody;
 
@@ -18,24 +18,23 @@ public class ball : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey(KeyCode.UpArrow))
         {
-            m_rigidBody.rotation = Quaternion.Euler(m_rigidBody.rotation.x, m_rigidBody.rotation.y, m_rigidBody.rotation.z + 1.5f);
-         //   transform.Rotate(0f, 0f, 1.5f);
+            transform.Rotate(1.5f, 0f, 0f);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Rotate(0f, 0f, -1.5f);
+            transform.Rotate(-1.5f, 0f, 0f);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(1.5f, 0f, 0f);
+            transform.Rotate(0f, 0f, -1.5f);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(-1.5f, 0f, 0f);
+            transform.Rotate(0f, 0f, 1.5f);
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            m_rigidBody.AddForce(-transform.right * thrust * Time.deltaTime);
+            m_rigidBody.AddForce(transform.forward * thrust, ForceMode.Impulse);
         }
 
 
