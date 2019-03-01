6df96b1c47944c8f632a7a3bc6b35260c664c62c
zack <zedoov@hotmail.com>
2019-03-01 16:06:28 +1100

    Start of UnityPhysics Ragdoll/playerController done

---
+++ PhysicsAssignment/Assets/scripts/ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    public float thrust;
+
+    public Rigidbody m_rigidBody;
+
+
+    // Use this for initialization
+    void Start () {
+        m_rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+        {
+            m_rigidBody.rotation = Quaternion.Euler(m_rigidBody.rotation.x, m_rigidBody.rotation.y, m_rigidBody.rotation.z + 1.5f);
+         //   transform.Rotate(0f, 0f, 1.5f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(0f, 0f, -1.5f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(1.5f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-1.5f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_rigidBody.AddForce(-transform.right * thrust * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ PhysicsAssignment/Assets/scripts/characterCon.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent (typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+
+
+public class characterCon : MonoBehaviour {
+    CharacterController controller = null;
+    Animator animator = null;
+
+
+    public float speed;
+    public float pushPower;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        controller.SimpleMove(transform.up * Time.deltaTime);
+        //transform.Rotate(transform.up, horizontal * speed * Time.deltaTime);
+        //animator.SetFloat("speed", vertical * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.up, horizontal * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.up, horizontal * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("speed", vertical * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetFloat("speed", vertical * speed * Time.deltaTime);
+        }
+	}
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if(body == null || body.isKinematic)
+        { return; }
+        if(hit.moveDirection.y < -0.3f)
+        { return; }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+}
