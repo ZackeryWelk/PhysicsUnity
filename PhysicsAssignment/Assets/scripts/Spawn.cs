3fad8178cb970421bb75ae4e131b82496b0439e3
zack <zedoov@hotmail.com>
2019-03-28 15:46:37 +1100

    finished assessment

---
+++ PhysicsAssignment/Assets/scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawn : MonoBehaviour {
+    public Text textbox;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int layermask = 1 << 8;
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit,Mathf.Infinity,layermask))
+        {
+            Debug.Log(hit.transform.position);
+            textbox.text = hit.transform.position.ToString();
+        }
+		
+	}
+}
